83293bcbd7b571201bfa2427b8a7927458490dd7
patnr <patrick.n.raanes@gmail.com>
2022-11-02 13:52:44 +0100

    Make simulator 2d

--- simulator.py
+++ simulator.py
@@ -10,7 +10,7 @@ def pressure_step(Grid, S, Fluid, q):
     # Compute K*lambda(S)
     Mw, Mo = RelPerm(S, Fluid)
     Mt = Mw+Mo
-    Mt = Mt.reshape((Grid.Nx, Grid.Ny, Grid.Nz), order="F")
+    Mt = Mt.reshape((Grid.Nx, Grid.Ny), order="F")
     KM = Mt*Grid.K
     # Compute pressure and extract fluxes
     [P, V] = TPFA(Grid, KM, q)
@@ -41,61 +41,48 @@ def TPFA(Grid,K,q):
     # Compute transmissibilities by harmonic averaging.
     Nx=Grid.Nx
     Ny=Grid.Ny
-    Nz=Grid.Nz
-    N=Nx*Ny*Nz
+    N = Nx * Ny
 
     hx=Grid.hx
     hy=Grid.hy
-    hz=Grid.hz
 
     L = K**(-1)
 
-    tx = 2*hy*hz/hx
-    TX = np.zeros((Nx+1,Ny,Nz))
+    tx = 2*hy/hx
+    TX = np.zeros((Nx+1, Ny))
 
-    ty = 2*hx*hz/hy
-    TY = np.zeros((Nx,Ny+1,Nz))
+    ty = 2*hx/hy
+    TY = np.zeros((Nx,Ny+1))
 
-    tz = 2*hx*hy/hz
-    TZ = np.zeros((Nx,Ny,Nz+1))
-
-
-    TX[1:-1,:,:] = tx/(L[0,:-1,:,:] + L[0,1:,:,:])
-    TY[:,1:-1,:] = ty/(L[1,:,:-1,:] + L[1,:,1:,:])
-    TZ[:,:,1:-1] = tz/(L[2,:,:,:-1] + L[2,:,:,1:])
-
+    TX[1:-1,:] = tx/(L[0,:-1,:] + L[0,1:,:])
+    TY[:,1:-1] = ty/(L[1,:,:-1] + L[1,:,1:])
 
     # Assemble TPFA discretization matrix.
-    x1 = TX[:-1,:,:].ravel(order="F")
-    x2 = TX[1:,:,:].ravel(order="F")
+    x1 = TX[:-1,:].ravel(order="F")
+    x2 = TX[1:,:].ravel(order="F")
 
-    y1 = TY[:,:-1,:].ravel(order="F")
-    y2 = TY[:,1:,:].ravel(order="F")
+    y1 = TY[:,:-1].ravel(order="F")
+    y2 = TY[:,1:].ravel(order="F")
 
-    z1 = TZ[:,:,:-1].ravel(order="F")
-    z2 = TZ[:,:,1:].ravel(order="F")
-
-
-    DiagVecs = [   -z2, -y2, -x2, x1+x2+y1+y2+z1+z2, -x1, -y1,  -z1]
-    DiagIndx = [-Nx*Ny, -Nx,  -1,         0        ,  1 ,  Nx, Nx*Ny]
-
+    DiagVecs = [-y2, -x2, x1+x2+y1+y2, -x1, -y1]
+    DiagIndx = [-Nx,  -1,         0  ,   1,  Nx]
+    DiagVecs[2][0] += np.sum(Grid.K[:,0,0])
 
     A = sparse.spdiags(DiagVecs, DiagIndx, N, N)
     A = A.toarray()
-    A[0, 0] = A[0, 0]+np.sum(Grid.K[:, 0, 0, 0])
 
     # Solve linear system and extract interface fluxes.
     u = np.linalg.solve(A, q)
-    P = u.reshape((Nx, Ny, Nz), order="F")
+    P = u.reshape((Nx, Ny), order="F")
 
     V = {}
-    V['x'] = np.zeros((Nx+1, Ny, Nz))
-    V['y'] = np.zeros((Nx, Ny+1, Nz))
-    V['z'] = np.zeros((Nx, Ny, Nz+1))
+    V = DotDict(
+        x=np.zeros((Nx+1, Ny)),
+        y=np.zeros((Nx, Ny+1)),
+    )
 
-    V['x'][1:-1, :, :] = (P[:-1, :, :] - P[1:, :, :]) * TX[1:-1, :, :]
-    V['y'][:, 1:-1, :] = (P[:, :-1, :] - P[:, 1:, :]) * TY[:, 1:-1, :]
-    V['z'][:, :, 1:-1] = (P[:, :, :-1] - P[:, :, 1:]) * TZ[:, :, 1:-1]
+    V.x[1:-1,:] = (P[:-1,:] - P[1:,:]) * TX[1:-1,:]
+    V.y[:,1:-1] = (P[:,:-1] - P[:,1:]) * TY[:,1:-1]
     return P, V
 
 
@@ -104,18 +91,15 @@ def upwind_diff(Grid, V, q):
     """Upwind finite-volume scheme."""
     Nx = Grid.Nx
     Ny = Grid.Ny
-    Nz = Grid.Nz
-    N = Nx*Ny*Nz
+    N = Nx*Ny
     fp = q.clip(max=0)  # production
     # Flow fluxes, separated into direction (x-y) and sign
-    XN=V['x'].clip(max=0); x1=XN[:-1, :, :].ravel(order="F")
-    YN=V['y'].clip(max=0); y1=YN[:,:-1,:]  .ravel(order="F")
-    ZN=V['z'].clip(max=0); z1=ZN[:,:,:-1]  .ravel(order="F")
-    XP=V['x'].clip(min=0); x2=XP[1:,:,:]   .ravel(order="F")
-    YP=V['y'].clip(min=0); y2=YP[:,1:,:]   .ravel(order="F")
-    ZP=V['z'].clip(min=0); z2=ZP[:,:,1:]   .ravel(order="F")
-    DiagVecs=[    z2,  y2, x2, fp+x1-x2+y1-y2+z1-z2, -x1, -y1, -z1]    # diagonal vectors
-    DiagIndx=[-Nx*Ny, -Nx, -1,           0         ,  1 ,  Nx, Nx*Ny]  # diagonal index
+    XN=V.x.clip(max=0); x1=XN[:-1,:].ravel(order="F") # separate flux into
+    YN=V.y.clip(max=0); y1=YN[:,:-1].ravel(order="F") # - flow in positive coordinate
+    XP=V.x.clip(min=0); x2=XP[1:,:] .ravel(order="F") # - flow in negative coordinate
+    YP=V.y.clip(min=0); y2=YP[:,1:] .ravel(order="F") #   direction (XN,YN)
+    DiagVecs=[    y2, x2, fp+x1-x2+y1-y2, -x1, -y1] # diagonal vectors
+    DiagIndx=[  -Nx , -1,        0      ,  1 ,  Nx] # diagonal index
     # matrix with upwind FV stencil
     A=sparse.spdiags(DiagVecs,DiagIndx,N,N)
     return A
@@ -125,8 +109,7 @@ def upwind_diff(Grid, V, q):
 def saturation_step_upwind(Grid,S,Fluid,V,q,T):
     Nx = Grid.Nx
     Ny = Grid.Ny
-    Nz = Grid.Nz                                        # number of grid points
-    N = Nx*Ny*Nz                                           # number of unknowns
+    N = Nx*Ny
     # pore volume=cell volume*porosity
     pv = Grid.V*Grid.por.ravel(order="F")
 
@@ -134,15 +117,11 @@ def saturation_step_upwind(Grid,S,Fluid,V,q,T):
     fi = q.clip(min=0)
 
     # In-/Out-flux x-/y- faces
-    XP=V['x'].clip(min=0)
-    XN=V['x'].clip(max=0)
-    YP=V['y'].clip(min=0)
-    YN=V['y'].clip(max=0)
-    ZP=V['z'].clip(min=0)
-    ZN=V['z'].clip(max=0)
-
-    Vi = XP[:-1,:,:]+YP[:,:-1,:]+ZP[:,:,:-1]- \
-         XN[ 1:,:,:]-YN[:, 1:,:]-ZN[:,:, 1:]  # each gridblock
+    XP = V.x.clip(min=0)
+    XN = V.x.clip(max=0)
+    YP = V.y.clip(min=0)
+    YN = V.y.clip(max=0)
+    Vi = XP[:-1, :] + YP[:, :-1] - XN[1:, :] - YN[:, 1:]
 
     # Compute dt
     pm = min(pv/(Vi.ravel(order="F")+fi))                  # estimate of influx
@@ -162,30 +141,22 @@ def saturation_step_upwind(Grid,S,Fluid,V,q,T):
 if __name__ == "__main__":
     # Settings as in listing 9
 
-    # Domain settings
-    Dx = 1
-    Dy = 1
-    Dz = 1
-
     # Grid settings
     Grid = DotDict(
         Dx=1,
         Dy=1,
-        Dz=1,
         Nx=64,
         Ny=64,
-        Nz=1,
     )
-    Grid.N = Grid.Nx * Grid.Ny * Grid.Nz
+    Grid.N = Grid.Nx * Grid.Ny
 
     # Cell dims
     Grid.hx  = Grid.Dx / Grid.Nx
     Grid.hy  = Grid.Dy / Grid.Ny
-    Grid.hz  = Grid.Dz / Grid.Nz
-    Grid.V   = Grid.hx * Grid.hy * Grid.hz
+    Grid.V   = Grid.hx * Grid.hy
 
-    Grid.K   = np.ones((3, Grid.Nx, Grid.Ny, Grid.Nz)) # Unit permeability
-    Grid.por = np.ones((Grid.Nx, Grid.Ny, Grid.Nz))    # Unit porosity
+    Grid.K   = np.ones((2, Grid.Nx, Grid.Ny)) # Unit permeability
+    Grid.por = np.ones((Grid.Nx, Grid.Ny))    # Unit porosity
 
     # Source terms: production/injection
     Q     = np.zeros(Grid.N)
